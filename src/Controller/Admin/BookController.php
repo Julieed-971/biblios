d5b05267997e68064df11cad2a281bcf569796ed
Julieed-971 <julie.dedieu1@gmail.com>
2024-10-03 10:45:59 +0200

    Added missing use statement and administration interface templates

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -9,9 +9,11 @@ use Doctrine\ORM\EntityManagerInterface;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/book')]
 class BookController extends AbstractController
--- src/Controller/Admin/EditorController.php
+++ src/Controller/Admin/EditorController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/editor')]
 class EditorController extends AbstractController
